9cc27ee8b8e5390dea44ac4f4a509ed8ed467be6
howardchi <howardchi@fougood.com>
2014-01-21 16:22:01 +0800

    add sample

--- libs/index.js
+++ libs/index.js
@@ -1,8 +1,21 @@
+var fs = require('fs');
 var xlsjs = require('xlsjs');
 
 xls_json = function(config) {
-  var str = xlsjs.readFile(config.input);
-  console.log(str);
+  if(!config.input) {
+    console.error("You must have to choose a file");
+  }
+
+  var wb = xlsjs.readFile(config.input);
+  var filename, sheetname = '', target_sheet = '';
+  if(target_sheet === '') 
+    target_sheet = wb.SheetNames[0];
+  ws = wb.Sheets[target_sheet];
+
+  //make csv
+  var csv_file = xlsjs.utils.make_csv(ws)
+  var stream = fs.createWriteStream(config.output, { flags : 'w' });
+  stream.write(csv_file);
 }
 
 module.exports = xls_json;
--- sample/sample.js
+++ sample/sample.js
@@ -1,5 +1,6 @@
 var xls_json = require('../')
 
 xls_json({
-  input: __dirname + '/number_format.xls'
+  input: __dirname + '/number_format.xls',
+  output: __dirname + '/test.csv'
 });
